195c1805c4c9172476464226c72a495cceb655b6
Dan <vareladanmarj@gmail.com>
2025-02-16 04:57:56 +0800

    nvim: treesitter-based folding

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,5 @@ vim.opt.foldlevel = 999
 vim.opt.conceallevel = 1
 vim.opt.wrap = true
 vim.opt.clipboard:append("unnamedplus")
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
