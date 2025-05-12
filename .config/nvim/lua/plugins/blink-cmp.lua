53926a6ffc0bbe310c666caae8f081b572a18a09
Dan <vareladanmarj@gmail.com>
2025-05-13 03:27:35 +0800

    nvim: ai integration test

---
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,17 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = {
+    "Kaiser-Yang/blink-cmp-avante",
+  },
+  opts = {
+    sources = {
+      default = { "avante" },
+      providers = {
+        avante = {
+          module = "blink-cmp-avante",
+          name = "Avante",
+        },
+      },
+    },
+  },
+}
