a2e1a016faa4135912eea2757dae2d6da898c023
Dan <vareladanmarj@gmail.com>
2025-05-14 01:35:21 +0800

    nvim: add avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,61 +1,62 @@
-return {}
--- return {
---   "yetone/avante.nvim",
---   event = "VeryLazy",
---   version = false, -- Never set this value to "*"! Never!
---   opts = {
---     provider = "openai",
---     file_selector = {
---       provider = "telescope",
---     },
---     openai = {
---       endpoint = "https://api.openai.com/v1",
---       model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
---       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
---       temperature = 0,
---       max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
---       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
---     },
---     repo_map = {
---       ignore_patterns = { "%.git", "%.worktree", "__pycache__", "node_modules", "dist" }, -- ignore files matching these
---     },
---     selector = {
---       provider = "snacks",
---     },
---   },
---   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
---   build = "make",
---   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
---   dependencies = {
---     "nvim-treesitter/nvim-treesitter",
---     "nvim-lua/plenary.nvim",
---     "MunifTanjim/nui.nvim",
---     --- The below dependencies are optional,
---     "nvim-telescope/telescope.nvim",
---     "hrsh7th/nvim-cmp",
---     "echasnovski/mini.icons",
---     {
---       -- support for image pasting
---       "HakonHarnes/img-clip.nvim",
---       event = "VeryLazy",
---       opts = {
---         -- recommended settings
---         default = {
---           embed_image_as_base64 = false,
---           prompt_for_file_name = false,
---           drag_and_drop = {
---             insert_mode = true,
---           },
---           use_absolute_path = true,
---         },
---       },
---     },
---     {
---       "MeanderingProgrammer/render-markdown.nvim",
---       opts = {
---         file_types = { "markdown", "Avante" },
---       },
---       ft = { "markdown", "Avante" },
---     },
---   },
--- }
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    provider = "claude",
+    file_selector = {
+      provider = "telescope",
+    },
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-7-sonnet-20250219",
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+      max_tokens = 20480,
+    },
+    repo_map = {
+      ignore_patterns = { "%.git", "%.worktree", "__pycache__", "node_modules", "dist" }, -- ignore files matching these
+    },
+    selector = {
+      provider = "snacks",
+    },
+    behaviour = {
+      minimize_diff = false,
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "echasnovski/mini.icons",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
