a8523134f0c5aa5b65a8ea1a8a9284a9dc629a87
Dan <vareladanmarj@gmail.com>
2025-05-29 02:12:33 +0800

    nvim: add codesnap, enable jk to escape

---
+++ .config/nvim/lazynvim/lua/plugins/codesnap.lua
@@ -0,0 +1,11 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make",
+  opts = {
+    save_path = "~/Pictures/Screenshots",
+    has_breadcrumbs = true,
+    has_line_number = true,
+    bg_padding = 12,
+    watermark = "",
+  },
+}
