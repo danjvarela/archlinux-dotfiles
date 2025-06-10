cdd471ee398ab4f5118a88456385d73fa3ab0fab
Dan <vareladanmarj@gmail.com>
2025-06-10 20:12:46 +0800

    nvim: disable bufferline, add nvim-orgmode

--- .config/nvim/lazynvim/lua/plugins/nvim-orgmode.lua
+++ .config/nvim/lazynvim/lua/plugins/nvim-orgmode.lua
@@ -1,10 +1,36 @@
 return {
   "nvim-orgmode/orgmode",
+  dependencies = {
+    "akinsho/org-bullets.nvim",
+  },
   event = "VeryLazy",
   config = function()
     require("orgmode").setup({
-      org_agenda_files = "~/.orgfiles/**/*",
+      org_agenda_files = "~/.orgfiles/**/*.org",
       org_default_notes_file = "~/.orgfiles/refile.org",
+      org_todo_keywords = {
+        "TODO",
+        "ONGOING",
+        "HOLD",
+        "WAITING",
+        "|",
+        "DONE",
+        "CANCELLED",
+      },
+      org_todo_keyword_faces = {
+        TODO = ":foreground red :weight bold",
+        ONGOING = ":foreground yellow :weight bold",
+        WAITING = ":foreground orange :weight bold",
+        HOLD = ":foreground magenta :weight bold",
+        DONE = ":foreground green :weight bold",
+        CANCELLED = ":foreground grey :weight bold",
+      },
+      ui = {
+        input = {
+          use_vim_ui = true,
+        },
+      },
     })
+    require("org-bullets").setup()
   end,
 }
