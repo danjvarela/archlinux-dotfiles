253f947f4f492b1716c2844c5fab1b539a798489
Dan <vareladanmarj@gmail.com>
2025-06-10 21:33:33 +0800

    nvim: add orgmode notification

---
+++ .config/nvim/lazynvim/lua/partials/org-cron.lua
@@ -0,0 +1,11 @@
+local orgmode = vim.fn.stdpath("data") .. "/lazy/orgmode"
+vim.opt.runtimepath:append(orgmode)
+
+-- Run the orgmode cron
+require("orgmode").cron({
+  org_agenda_files = "~/.orgfiles/**/*.org",
+  org_default_notes_file = "~/.orgfiles/refile.org",
+  notifications = {
+    reminder_time = { 0, 5, 10 },
+  },
+})
--- .config/nvim/lazynvim/lua/plugins/nvim-orgmode.lua
+++ .config/nvim/lazynvim/lua/plugins/nvim-orgmode.lua
@@ -50,6 +50,25 @@ return {
           },
         },
       },
+      notifications = {
+        enabled = true,
+        reminder_time = { 0, 5, 10 },
+        notifier = function(tasks)
+          local result = {}
+          for _, task in ipairs(tasks) do
+            require("orgmode.utils").concat(result, {
+              string.format("# %s (%s)", task.category, task.humanized_duration),
+              string.format("%s %s %s", string.rep("*", task.level), task.todo, task.title),
+              string.format("%s: <%s>", task.type, task.time:to_string()),
+            })
+          end
+
+          -- Example: if you use Snacks, you can do something like this (THis is not implemented)
+          Snacks.notifier.notify(table.concat(result, "\n"), vim.log.levels.INFO, {
+            title = "Orgmode",
+          })
+        end,
+      },
     })
     require("org-bullets").setup()
   end,
