24880532475c76de0f492df49edfffc9fb4d9235
Dan <vareladanmarj@gmail.com>
2026-03-03 17:11:36 +0800

    nvim: add markdown viewing keymaps

--- .config/nvim/v3/lua/plugins/viewing-markdown.lua
+++ .config/nvim/v3/lua/plugins/viewing-markdown.lua
@@ -3,5 +3,13 @@ vim.pack.add({
 })
 require("render-markdown").setup({
 	enabled = false,
-  preset = "obsidian"
+	preset = "obsidian",
+  anti_conceal = {
+    enabled = false
+  }
 })
+
+local render_markdown = require("render-markdown")
+
+vim.keymap.set("n", "<leader>mm", render_markdown.buf_toggle, { desc = "View markdown" })
+vim.keymap.set("n", "<leader>mp", render_markdown.preview, { desc = "View markdown (Split)" })
