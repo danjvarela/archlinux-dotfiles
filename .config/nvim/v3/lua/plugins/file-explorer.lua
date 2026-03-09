e69cd17af9b1f3819303acb5fea776cea52e8123
Dan <vareladanmarj@gmail.com>
2026-03-09 22:09:33 +0800

    nvim: replace file explorer and cursor jumping plugins

--- .config/nvim/v3/lua/plugins/file-explorer.lua
+++ .config/nvim/v3/lua/plugins/file-explorer.lua
@@ -1,62 +1,52 @@
 vim.pack.add({
-	"https://github.com/JezerM/oil-lsp-diagnostics.nvim",
-	{
-		src = "https://github.com/malewicz1337/oil-git.nvim",
-		version = vim.version.range("^v1.0.1"),
-	},
-	{
-		src = "https://github.com/stevearc/oil.nvim",
-		version = vim.version.range("^v2.15.0"),
-	},
+	{ src = "https://github.com/A7Lavinraj/fyler.nvim", version = vim.version.range("^v2.0.0") },
 })
 
-local detail = false
-local oil = require("oil")
-oil.setup({
-	delete_to_trash = true,
-	view_options = {
-		show_hidden = true,
-	},
-	win_options = {
-		signcolumn = "yes:2",
-	},
-	git = {
-		mv = function()
-			return true
-		end,
-		rm = function()
-			return true
-		end,
-	},
-	lsp_file_methods = {
-		-- Enable or disable LSP file operations
-		enabled = true,
-		-- Time to wait for LSP file operations to complete before skipping
-		timeout_ms = 1000,
-		-- Set to true to autosave buffers that are updated with LSP willRenameFiles
-		-- Set to "unmodified" to only save unmodified buffers
-		autosave_changes = true,
-	},
-	watch_for_changes = false,
-	skip_confirm_for_simple_edits = true,
-	keymaps = {
-		["gd"] = {
-			desc = "Toggle file detail view",
-			callback = function()
-				detail = not detail
-				if detail then
-					require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
-				else
-					require("oil").set_columns({ "icon" })
-				end
-			end,
+local fyler = require("fyler")
+
+fyler.setup({
+	views = {
+		finder = {
+			win = {
+				kinds = {
+					split_left_most = {
+						width = "20%",
+						win_opts = {
+							winfixwidth = true,
+						},
+					},
+				},
+			},
+			watcher = { enabled = true },
+			columns_order = { "diagnostic", "git" },
+			columns = {
+				git = {
+					enabled = true,
+					symbols = {
+						Untracked = "",
+						Added = "",
+						Modified = "",
+						Deleted = "",
+						Renamed = "",
+						Copied = "",
+						Conflict = "",
+						Ignored = "",
+					},
+				},
+				diagnostic = {
+					enabled = "true",
+					symbols = {
+						Error = "",
+						Warn = "",
+						Info = "",
+						Hint = "",
+					},
+				},
+			},
 		},
 	},
 })
 
-require("oil-git").setup({
-	symbol_position = "signcolumn",
-})
-require("oil-lsp-diagnostics").setup()
-
-vim.keymap.set({ "n", "v" }, "<leader>e", oil.toggle_float, { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>e", function()
+	fyler.toggle({ kind = "split_left_most" })
+end, { desc = "Open File Explorer" })
--- .config/nvim/v3/lua/plugins/jumping.lua
+++ .config/nvim/v3/lua/plugins/jumping.lua
@@ -1,12 +1,25 @@
 vim.pack.add({
 	{
-		src = "https://github.com/nvim-mini/mini.jump2d",
-    version = "main",
+		src = "https://github.com/folke/flash.nvim",
+		version = vim.version.range("^v2.1.0"),
 	},
 })
 
-require("mini.jump2d").setup({
-  view = {
-    dim = true
-  }
-})
+require("flash").setup()
+
+-- Keymaps for flash.nvim
+vim.keymap.set({ "n", "x", "o" }, "s", function()
+	require("flash").jump()
+end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function()
+	require("flash").treesitter()
+end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "r", function()
+	require("flash").remote()
+end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function()
+	require("flash").treesitter_search()
+end, { desc = "Treesitter Search" })
+vim.keymap.set("c", "<c-s>", function()
+	require("flash").toggle()
+end, { desc = "Toggle Flash Search" })
