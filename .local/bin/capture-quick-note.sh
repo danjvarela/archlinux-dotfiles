4ce2fd47cb3b08907d6b4302f052ba4e9f869938
Dan <vareladanmarj@gmail.com>
2026-06-23 17:30:12 +0800

    kitty: update quick note capture script

--- .local/bin/capture-quick-note.sh
+++ .local/bin/capture-quick-note.sh
@@ -3,5 +3,5 @@
 export HOME="/home/dan"
 export NVIM_APPNAME="nvim/v3"
 
-cd ~/Documents/notes
-kitten quick-access-terminal nvim quick/"$(date +"%Y-%m-%d %H:%M").md"
+cd ~/Documents/second-brain/
+kitten quick-access-terminal nvim inbox.md
